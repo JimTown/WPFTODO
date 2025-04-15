1c81dda0f557e29def247c858a1380346a39fbd9
JimTown <103100951+JimTown@users.noreply.github.com>
2025-04-15 14:57:21 +0800

    添加项目文件。

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WPFTODO.Model;
+
+namespace WPFTODO.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _newTaskName;
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+
+        public string NewTaskName
+        {
+            get => _newTaskName;
+            set
+            {
+                _newTaskName = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// 添加命令
+        /// </summary>
+        public RelayCommand AddTaskCommand { get; }
+
+        /// <summary>
+        /// 删除命令
+        /// </summary>
+        public RelayCommand<TaskItem> RemoveTaskCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            Tasks  = new ObservableCollection<TaskItem>();
+            AddTaskCommand = new RelayCommand(AddTask);
+            RemoveTaskCommand = new RelayCommand<TaskItem>(RemoveTask);
+        }
+
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskName))
+            {
+                Tasks.Add(new TaskItem { TaskName = NewTaskName });
+                NewTaskName = "";
+            }
+        }
+
+        private void RemoveTask(TaskItem task)
+        {
+           
+            Tasks.Remove(task);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
